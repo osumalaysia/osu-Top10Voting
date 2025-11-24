b8193bd83f675c4e245fc0e60fd07d11bde21dd9
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-11-24 14:38:26 +0800

    change the voting condition issue

--- apps/frontend/src/views/Vote.vue
+++ apps/frontend/src/views/Vote.vue
@@ -96,7 +96,7 @@
 
     votingCounts = false;
     multiplier = 0;
-    dateLimit: Date = new Date("2024-11-24T15:59:00Z");
+    dateLimit: Date = new Date('2024-11-24T15:59:00+00:00');
 
     // Votes
     first = null;
@@ -138,7 +138,7 @@
     }
 
     setMultiplierAndAbilityToVote() {
-      this.votingCounts = new Date(this.currentUser.account_createdAt) >= this.dateLimit;
+      this.votingCounts = new Date(this.currentUser.account_createdAt) <= this.dateLimit;
       if (this.votingCounts) {
           this.multiplier = 1;
         }
